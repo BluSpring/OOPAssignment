817efa2ebb7813810703d9c1f6eaaf630cca536b
Naz <bluspringindohq@gmail.com>
2025-05-02 13:04:09 +0800

    please render,,

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,86 @@
 import admin.Admin;
+import auth.AccountType;
 import customer.Customer;
 import seller.Seller;
+import util.ColorUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Main {
+    private static final JFrame frame = new JFrame() {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+
+            var g2d = (Graphics2D) g;
+            // Makes the rendering use "quality" rendering
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+            var width = this.getWidth();
+            var height = this.getHeight();
+            var gradient = new GradientPaint(0f, 0f, ColorUtils.fromHex(0x2c70f0), 0f, height, ColorUtils.fromHex(0x25358e));
+
+            g2d.setPaint(gradient);
+            g2d.fillRect(0, 0, width, height);
+        }
+
+        @Override
+        protected void processKeyEvent(KeyEvent e) {
+            super.processKeyEvent(e);
+
+            // Forced reload
+            if (e.getKeyCode() == KeyEvent.VK_F5) {
+                createLogin();
+            }
+        }
+    };
+
+    public static JFrame getFrame() {
+        return frame;
+    }
+
+    public static void reset() {
+        frame.removeAll();
+    }
+
+    public static void refresh() {
+        frame.invalidate();
+        frame.validate();
+        frame.repaint();
+    }
+
+    public static void createLogin() {
+        reset();
+
+        var mainPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+
+        {
+            var panel = new JPanel(new FlowLayout());
+
+            panel.add(new JLabel("Login as: "));
+            panel.add(new JComboBox<>(AccountType.values()));
+
+            mainPanel.add(panel);
+        }
+
+        frame.add(mainPanel);
+
+        refresh();
+    }
+
     public static void main(String[] args) {
+        frame.setSize(843, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         Admin.init();
         Customer.init();
         Seller.init();
+
+        createLogin();
     }
 }
---
+++ src/util/ColorUtils.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.awt.*;
+
+public class ColorUtils {
+    /**
+     * Converts a hex value to an AWT Color.
+     * @param hexRGB The hex value (formatted in RGB) to convert
+     * @return The AWT Color with the specified values
+     */
+    public static Color fromHex(int hexRGB) {
+        var r = (hexRGB >> 16) & 255;
+        var g = (hexRGB >> 8) & 255;
+        var b = hexRGB & 255;
+
+        return fromRGB(r, g, b);
+    }
+
+    /**
+     * Converts RGB color data to an AWT Color.
+     * @param r The red value (0-255)
+     * @param g The green value (0-255)
+     * @param b The blue value (0-255)
+     * @return The AWT Color with the specified values
+     */
+    public static Color fromRGB(int r, int g, int b) {
+        var hsb = Color.RGBtoHSB(r, g, b, null);
+        return Color.getHSBColor(hsb[0], hsb[1], hsb[2]);
+    }
+}
